e267cb40cafd3d2408014198d2aff99af8677f37
Selvin Selbaraju <selvin@ellas-mbp.home>
2021-08-26 12:33:34 +0100

    main section done

--- frontend/src/Presentation/Presentation.js
+++ frontend/src/Presentation/Presentation.js
@@ -9,12 +9,12 @@ function Presentation() {
                 <div className="introduction">
                     <h2 className="brand-title">Pretty Pretzels</h2>
                     <p className="intro-text">
-                        The tastiest, healthiest, most concious pretzels you can buy.
+                        The tastiest, healthiest, <span>most concious</span> pretzels you can buy.
                         We come in any kind of flavour you can think of!
                     </p>
                     <div className="cta">
-                        <button className="cta-more">Find Out More</button>
-                        <button className="cta-buy">Buy Now</button>
+                        <button className="cta-more"><a href="#">Find Out More</a></button>
+                        <button className="cta-buy"><a href="#">Buy Now</a></button>
                     </div>
                 </div>
                 <img  className="pretzel-picture" src={pretzels} alt="Pretzels" />
