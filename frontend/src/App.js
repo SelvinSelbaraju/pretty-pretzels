c6c334c953805f4bbc218237da6ea7e918c3721d
Selvin Selbaraju <selvin@ellas-mbp.home>
2021-08-26 12:03:05 +0100

    skeleton-structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './Navbar/Navbar';
+import Presentation from './Presentation/Presentation';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Presentation />
     </div>
   );
 }
--- frontend/src/Presentation/Presentation.js
+++ frontend/src/Presentation/Presentation.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import './Presentation.css'
+import pretzels from './pretzels.png'
 
 function Presentation() {
     return (
         <main>
-            
+            <div className="presentation">
+                <div className="introduction">
+                    <h2 className="brand-title">Pretty Pretzels</h2>
+                    <p className="intro-text">
+                        The tastiest, healthiest, most concious pretzels you can buy.
+                        We come in any kind of flavour you can think of!
+                    </p>
+                    <div className="cta">
+                        <button className="cta-more">Find Out More</button>
+                        <button className="cta-buy">Buy Now</button>
+                    </div>
+                </div>
+                <img  className="pretzel-picture" src={pretzels} alt="Pretzels" />
+            </div>
         </main>
     )
 }
