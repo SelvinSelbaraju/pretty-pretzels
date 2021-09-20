305891603dfb76905742d5f0f07aa65c282e6a0f
Selvin Selbaraju <selvin@ellas-mbp.home>
2021-09-20 16:39:25 +0100

    finished basic skeleton frontend

--- frontend/src/Dashboard/Dashboard.js
+++ frontend/src/Dashboard/Dashboard.js
@@ -1,28 +1,17 @@
-import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { logoutUser } from '../actions/authActions';
 import './Dashboard.css';
 
 function Dashboard(props) {
-    const history = useHistory();
-    const onLogoutClick = e => {
-        e.preventDefault();
-        props.logoutUser();
-        history.go(0)
-    };
-
     const user = props.auth.user;
     return (
         <div className="dashboard-container">
             <h2 className="welcome-title">{user.name ? `Welcome back ${user.name.split(" ")[0]}!` : "Goodbye!"}</h2>
-            <button className="logout-button" onClick={onLogoutClick}>Logout</button>
         </div>
     )
 }
 
 Dashboard.propTypes = {
-    logoutUser: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired
 };
 
@@ -30,4 +19,4 @@ const mapStateToProps = state => ({
     auth: state.auth
 })
 
-export default connect(mapStateToProps, { logoutUser })(Dashboard)
+export default connect(mapStateToProps)(Dashboard)
--- frontend/src/Navbar/Navbar.js
+++ frontend/src/Navbar/Navbar.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { logoutUser } from '../actions/authActions';
+import PropTypes from 'prop-types';
 import { useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import './Navbar.css';
 import logo from './logo.svg';
 import cart from './cart.svg';
 import circle from './Ellipse 1.svg'
 
 
-function Navbar() {
+function Navbar(props) {
     console.log(localStorage.jwtToken);
+    console.log(props.auth);
     const [circleHover, setCircleHover] = useState(false); 
 
     const handleCircleHover = () => {
@@ -18,6 +22,13 @@ function Navbar() {
     const handleCircleLeave = () => {
         setCircleHover(false);
     }
+
+    const history = useHistory();
+    const onLogoutClick = e => {
+        e.preventDefault();
+        props.logoutUser();
+        history.go(0)
+    };
 
     return (
             <header>
@@ -30,8 +41,18 @@ function Navbar() {
                         <li><Link to="/" className="nav-link">Home</Link></li>
                         <li><Link to="/" className="nav-link">About</Link></li>
                         <li><Link to="/" className="nav-link">Products</Link></li>
-                        <li><Link to="/login" className="nav-link">Login</Link></li>
-                        <li><Link to="/register" className="nav-link">Register</Link></li>
+                        {
+                            props.auth.isAuthenticated ?
+                            <>
+                                <li><Link to="/dashboard" className="nav-link dashboard">Dashboard</Link></li>
+                                <li className="nav-link logout" onClick={onLogoutClick}>Logout</li>
+                            </>
+                        :
+                            <>
+                                <li><Link to="/login" className="nav-link">Login</Link></li>
+                                <li><Link to="/register" className="nav-link">Register</Link></li>
+                            </>
+                        }
                     </ul>
                 </nav>
                 <div className="cart-container" onMouseEnter={handleCircleHover} onMouseLeave={handleCircleLeave}>
@@ -42,4 +63,13 @@ function Navbar() {
     )
 }
 
-export default Navbar
+Navbar.propTypes = {
+    logoutUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth
+})
+
+export default connect(mapStateToProps, { logoutUser})(Navbar)
